0af24a95a046a352b9db9882d7420fd6ca38c860
Jens Ochsenmeier <github@jens-ox.de>
2019-01-10 12:47:30 +0100

    add support for y translations and slot usages

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,44 +1,53 @@
 <template>
-  <svg class="block" style="min-height: 270px" :style="{
-    'width': width
+  <svg class="block" :style="{
+    'width': width,
+    'min-height': minHeight
   }">
     <g :transform='`translate(${30},${30})`'>
+      <slot />
       <line
         v-for='(edge, i) in edges'
         :key='i'
-        :x1='(edge.from.x - leftMax) * scaling'
-        :y1='edge.from.y * scaling'
-        :x2='(edge.to.x - leftMax) * scaling'
-        :y2='edge.to.y * scaling'
+        :x1='(edge.from.x - minX) * scaling'
+        :y1='(edge.from.y - minY) * scaling'
+        :x2='(edge.to.x - minX) * scaling'
+        :y2='(edge.to.y - minY) * scaling'
         class="tree-line"
       />
-            <g
-              v-for='vertex in vertices'
-              :key='vertex.name'
-            >
-              <circle
-                :cx='(vertex.x - leftMax) * scaling'
-                :cy='vertex.y * scaling'
-                :r='circleRadius'
-                class="tree-circle"
-                :class="{
-                  'tree-circle-marked-red': vertex.marked && vertex.marked === 'red',
-                  'tree-circle-marked-green': vertex.marked && vertex.marked === 'green'
-                }"
-              />
-              <text
-                :x="(vertex.x - leftMax) * scaling"
-                :y="vertex.y * scaling + 1"
-                class="tree-text"
-              >{{ vertex.name }}</text>
-            </g>
+      <g
+        v-for='vertex in vertices'
+        :key='vertex.name'
+      >
+        <circle
+          :cx='(vertex.x - minX) * scaling'
+          :cy='(vertex.y - minY) * scaling'
+          :r='circleRadius'
+          class="tree-circle"
+          :class="{
+            'tree-circle-marked-red': vertex.marked && vertex.marked === 'red',
+            'tree-circle-marked-green': vertex.marked && vertex.marked === 'green'
+          }"
+        />
+        <text
+          :x="(vertex.x - minX) * scaling"
+          :y="(vertex.y - minY) * scaling + 1"
+          class="tree-text"
+        >{{ vertex.name }}</text>
+      </g>
     </g>
   </svg>
 </template>
 <script>
 export default {
   props: {
-    leftMax: Number,
+    minX: {
+      type: Number,
+      default: 0
+    },
+    minY: {
+      type: Number,
+      default: 0
+    },
     scaling: {
       type: Number,
       default: 40
@@ -50,6 +59,10 @@ export default {
     width: {
       type: String,
       default: '100%'
+    },
+    minHeight: {
+      type: String,
+      default: '270px'
     },
     edges: Array,
     vertices: Array
